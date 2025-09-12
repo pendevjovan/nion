c4ef664fd58e4bead17a0b5d29fe6f278e26c04a
Jovan Pendev <jovan.pendev@nionit.com>
2025-09-12 09:59:35 +0200

    initial

--- src/PrinterTask.java
+++
@@ -1,8 +0,0 @@
-// TODO: Define PrinterTask class that implements Runnable
-class PrinterTask implements Runnable {
-    public void run() {
-        for (int i = 1; i < 6; i++) {
-            System.out.println(Thread.currentThread().getName() + " Running task " + i);
-        }
-    }
-}
